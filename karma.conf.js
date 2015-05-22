4d752834b556fc02a91b717397e9a46c3b7f2f2b
Paul Hammond <Paul.Hammond@bbc.co.uk>
2015-05-23 00:35:50 +0100

    karma ci changes

--- karma.conf.js
+++ karma.conf.js
@@ -33,9 +33,12 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['dots', 'junit'],
 
 
+    junitReporter: {
+        outputFile: 'test-results.xml'
+    },
     // web server port
     port: 9876,
 
@@ -60,6 +63,6 @@ module.exports = function(config) {
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false
+    singleRun: true
   });
 };
