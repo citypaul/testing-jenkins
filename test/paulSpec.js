049149b85b3af44272638c488c3e95ad35fe295a
Paul Hammond <paul.hammond@gmail.com>
2015-05-22 19:12:49 -0400

    added a passing test

--- test/paulSpec.js
+++ test/paulSpec.js
@@ -2,4 +2,9 @@ describe('Some tests', function() {
 	it('should fail', function() {
 		expect(true).toBeFalsy();
 	});
+	
+	it('should pass', function () {
+		expect(true).toBeTruthy();
+	});
+
 });
