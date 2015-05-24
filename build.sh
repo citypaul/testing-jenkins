99be5dddc85cfef224cd4c753b0aaa454166e88f
Paul Hammond <Paul.Hammond@bbc.co.uk>
2015-05-25 00:34:30 +0100

    outputting jasmine-node junit xml file

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 rm -rf node_modules && npm cache clean && npm install
 
-node_modules/jasmine-node/bin/jasmine-node test/
+node_modules/jasmine-node/bin/jasmine-node --junitreport test-results.xml test/
 ./node_modules/grunt-cli/bin/grunt hint
