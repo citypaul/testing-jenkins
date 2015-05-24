43f1acad6a97a6f952ab1db8ab54149121bd6790
Paul Hammond <Paul.Hammond@bbc.co.uk>
2015-05-24 16:12:00 +0100

    build

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-rm -rf node_modules
-npm install
+rm -rf node_modules && npm cache clean && npm install
+
 ./node_modules/karma/bin/karma start
 ./node_modules/grunt-cli/bin/grunt hint
