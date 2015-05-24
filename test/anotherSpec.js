ccefc9ac5145f516557e0e94f7e7a1fc31a0dcc0
Paul Hammond <Paul.Hammond@bbc.co.uk>
2015-05-24 22:39:08 +0100

    shouldn't buld master on pull request build now

--- test/anotherSpec.js
+++ test/anotherSpec.js
@@ -3,10 +3,10 @@ describe('Oh look more tests', function() {
 
     it('should bomb out', function() {
         expect(true).toBeTruthy();
-    }); 
+    });
 
     it('should all be fine x', function () {
         expect(true).toBeTruthy();
-    });
+    });  
 
 });
