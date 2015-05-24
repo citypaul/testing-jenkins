c713f2a474e86b68d150205319ec97b3b1b657fe
Paul Hammond <Paul.Hammond@bbc.co.uk>
2015-05-25 00:30:21 +0100

    using jasmine-node for tests

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 rm -rf node_modules && npm cache clean && npm install
 
-./node_modules/karma/bin/karma start
+node_modules/jasmine-node/bin/jasmine-node test/
 ./node_modules/grunt-cli/bin/grunt hint
