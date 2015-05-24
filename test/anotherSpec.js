18e2974b80368770710a6abd2acb7eaf43a6c60b
Paul Hammond <Paul.Hammond@bbc.co.uk>
2015-05-24 22:34:24 +0100

    asd

--- test/anotherSpec.js
+++ test/anotherSpec.js
@@ -3,7 +3,7 @@ describe('Oh look more tests', function() {
 
     it('should bomb out', function() {
         expect(true).toBeTruthy();
-    });
+    }); 
 
     it('should all be fine x', function () {
         expect(true).toBeTruthy();
