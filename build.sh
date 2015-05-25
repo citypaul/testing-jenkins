9ac0db9f73d07328c7e71188949e8a16cc7b984f
Paul Hammond <Paul.Hammond@bbc.co.uk>
2015-05-25 22:11:38 +0100

    adding jscs to build

--- build.sh
+++ build.sh
@@ -3,3 +3,4 @@ rm -rf node_modules && npm install
 
 node_modules/jasmine-node/bin/jasmine-node --junitreport test/
 ./node_modules/grunt-cli/bin/grunt hint
+./node_modules/jscs/bin/jscs test/ src/js/
