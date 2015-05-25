65938c876656703d3dadab44de37bbd44ca0834b
Paul Hammond <Paul.Hammond@bbc.co.uk>
2015-05-25 01:08:36 +0100

    test should pass but not so sure about jshit

--- test/chapter2/chessBoardSpec.js
+++ test/chapter2/chessBoardSpec.js
@@ -1,9 +1,18 @@
 var chessBoard = require('../../src/js/chapter2/chessBoard'),
     board = chessBoard();
-"use strict";
+
 
 describe('Chessboard', function () {
-   it('should fail', function () {
-      expect(board.generateBoard()).toEqual("# # # #\n # # # #\n# # # #\n # # # #\n# # # #\n # # # #\n# # # #\n # # # #\n");
-   });
+    "use strict";
+    
+    it('should fail', function () {
+        expect(board.generateBoard()).toEqual("# # # #\n" +
+        " # # # #\n" +
+        "# # # #\n" +
+        " # # # #\n" +
+        "# # # #\n" +
+        " # # # #\n" +
+        "# # # #\n" +
+        " # # # #\n");
+    });
 });
