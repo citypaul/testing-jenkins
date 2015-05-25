b9177565af4376f6d7604582defced6a4bd2b183
Paul Hammond <Paul.Hammond@bbc.co.uk>
2015-05-25 22:20:50 +0100

    echoing out in the build script

--- build.sh
+++ build.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
+echo "installing npm modules"
 rm -rf node_modules && npm install
 
+echo "Running jasmine node tests"
+
 node_modules/jasmine-node/bin/jasmine-node --junitreport test/
+
+echo "Running jshint"
 ./node_modules/grunt-cli/bin/grunt hint
+
+echo "Running jscs"
 ./node_modules/jscs/bin/jscs test/ src/js/
